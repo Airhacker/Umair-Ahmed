f7611d688b0b43ed13aad5f3a3471d4b4d383d21
Umair Ahmed <umairahmed.dev@gmail.com>
2020-04-08 20:39:14 -0700

    Projects page

--- scripts/app.js
+++ scripts/app.js
@@ -1,59 +1,57 @@
 // Home Page Nav
 
 // Mobile Menu
-const menu = document.querySelector('#home-menu');
+const menu = document.querySelector("#home-menu");
 const nav = document.querySelector(".home-nav");
 
 let menuOpen = false;
 
-menu.addEventListener('click', () => {
-    if(menuOpen === false){
-        menuOpen = true;
-        nav.style.opacity = "1";
-        nav.style.pointerEvents = "all";
-        menu.innerText = "Close";
-    }else if(menuOpen === true){
-        menuOpen = false;
-        nav.style.opacity = "0";
-        nav.style.pointerEvents = "none";
-        menu.innerText = "Menu";
-    }
-})
+menu.addEventListener("click", () => {
+  if (menuOpen === false) {
+    menuOpen = true;
+    nav.style.opacity = "1";
+    nav.style.pointerEvents = "all";
+    menu.innerText = "Close";
+  } else if (menuOpen === true) {
+    menuOpen = false;
+    nav.style.opacity = "0";
+    nav.style.pointerEvents = "none";
+    menu.innerText = "Menu";
+  }
+});
 
 // Home page animations
 
 //Hero Description Animation
-const tl = new gsap.timeline(); 
+const tl = new gsap.timeline();
 
 tl.from(".hero-description", {
-   y:100,
-   opacity:0,
-   delay: 0.25,
-    stagger: {
-        amount: 1,
-        ease: "ease",
-    },
+  y: 100,
+  opacity: 0,
+  delay: 0.25,
+  stagger: {
+    amount: 1,
+    ease: "ease",
+  },
 });
 tl.from(".hero-image", {
-    duration:1,
-    opacity: 0, 
-    scale: 3
+  duration: 1,
+  opacity: 0,
+  scale: 3,
 });
 
 // Project Page Animations
-// Start Animation 
+// Start Animation
 
-const projectBtn = document.querySelector('.project-btn');
-const heading = document.querySelector('.project-heading');
+const projectBtn = document.querySelectorAll(".project-btn");
 
-projectBtn.addEventListener('click', () => {
-    heading.style.display = 'block';
-    heading.style.color = "white";
-    tl.to('.project-heading', {
-        scale: 1,
+projectBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    tl.to("#home-page", {
+      opacity: 0,
+      duration: 0.5,
+    }).to("#projects-page", {
+      y: -window.innerHeight,
     });
-    heading.style.position = 'initial';
-})
-
-
-
+  });
+});
